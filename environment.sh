b0680bd9c94c4ed690a6e878b95f176c51973d09
Tony Duan <tony84727@gmail.com>
2021-12-07 22:49:27 +0800

    feat: allow mc alias to connect to different nodes

--- environment.sh
+++ environment.sh
@@ -1,4 +1,8 @@
 # convenient alias for development
 alias dc='docker compose'
-alias mc='docker compose exec manage sh'
+#alias mc='docker compose exec manage sh'
 alias cc='docker compose exec control sh'
+
+function mc() {
+	docker compose exec manage${1-} sh
+}

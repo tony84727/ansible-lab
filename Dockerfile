867d245dd951e961cf696b44131ada6070d418bd
Tony Duan <tony84727@gmail.com>
2022-09-13 17:01:22 +0800

    feat: kustomize

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM alpine:3.15.0
-
-RUN apk add ansible openssh
-
-ADD --chmod=0755 entrypoint.sh /entrypoint.sh
-
-ENTRYPOINT [ "/entrypoint.sh" ]
-CMD ["sh"]
--- environment.sh
+++
@@ -1,8 +0,0 @@
-# convenient alias for development
-alias dc='docker compose'
-#alias mc='docker compose exec manage sh'
-alias cc='docker compose exec control sh'
-
-function mc() {
-	docker compose exec manage${1-} sh
-}
